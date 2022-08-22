d7e22b708c86ef007bef09228728d68529729079
牧大佑 <daisukemaki1003@gmail.com>
2022-08-22 15:39:50 +0900

    [add] 特定プロパティのオブジェクトに当たり判定を追加

--- lib/widget/map_chip.dart
+++ lib/widget/map_chip.dart
@@ -6,51 +6,60 @@ import 'package:tiled/tiled.dart';
 import 'package:flame/palette.dart';
 
 import '../gloabal.dart';
-import '../screen/game_main.dart';
 
-class MapChip extends Component with HasGameRef, CollisionCallbacks {
+class MapChip extends PositionComponent with HasGameRef, CollisionCallbacks {
   // マップパス
   String mapPath = "";
   // 1フレームのスプライトサイズ
   Vector2 spriteSize = Vector2(32.0, 32.0);
 
-  TiledComponent? mapChip;
+  // マップタイル
+  TiledComponent? mapTiled;
 
   MapChip(this.mapPath, this.spriteSize);
 
   @override
   Future<void>? onLoad() async {
-    mapChip = await TiledComponent.load(mapPath, spriteSize);
-    await add(mapChip!);
+    // マップタイル
+    mapTiled = await TiledComponent.load(mapPath, spriteSize);
+    await add(mapTiled!);
 
-    // マップチップの情報から当たり判定を作る
-    TileLayer? hitLayer = mapChip!.tileMap.getLayer("wall");
-    int line = 0;
-    int column = 0;
-    hitLayer!.tileData!.forEach((element) {
-      element.forEach((childElement) {
-        if (childElement.tile == 343) {
-          // 通れる地面以外の場合当たり判定を置く
-          PositionComponent hitSprite = PositionComponent(
-            position: Vector2(spriteSize.x * column, spriteSize.y * line),
-            size: spriteSize,
-          );
-          RectangleHitbox rectangleHitbox = RectangleHitbox(
-            position: Vector2.zero(),
-            size: spriteSize,
-          );
-          hitSprite.add(rectangleHitbox);
-          add(hitSprite);
-          if (!isRelease) {
-            rectangleHitbox.renderShape = true;
-            rectangleHitbox.paint = BasicPalette.red.withAlpha(100).paint();
+    /// マップチップの情報から当たり判定を作る
+    /// for レイヤー
+    for (var layer in mapTiled!.tileMap.renderableLayers) {
+      print(layer.layer.name);
+      print(layer.layer.properties);
+      // "collision"ならヒットボックス配置
+      if (layer.layer.properties.isNotEmpty &&
+          layer.layer.properties[0].name.contains("collision")) {
+        TileLayer? hitLayer = mapTiled!.tileMap.getLayer(layer.layer.name);
+        final tileData = hitLayer!.tileData;
+        for (var line = 0; line < tileData!.length; line++) {
+          for (var column = 0; column < tileData[0].length; column++) {
+            final tile = tileData[line][column].tile;
+            if (tile > 0) {
+              // オブジェクトの位置
+              PositionComponent hitSprite = PositionComponent(
+                position: Vector2(spriteSize.x * column, spriteSize.y * line),
+                size: spriteSize,
+              );
+              // ヒットボックス
+              RectangleHitbox rectangleHitbox = RectangleHitbox(
+                position: Vector2.zero(),
+                size: spriteSize,
+              );
+              hitSprite.add(rectangleHitbox);
+              add(hitSprite);
+              // デバッグモード
+              if (!isRelease) {
+                rectangleHitbox.renderShape = true;
+                rectangleHitbox.paint = BasicPalette.red.withAlpha(100).paint();
+              }
+            }
           }
         }
-        column++;
-      });
-      column = 0;
-      line++;
-    });
+      }
+    }
     await super.onLoad();
   }
 
